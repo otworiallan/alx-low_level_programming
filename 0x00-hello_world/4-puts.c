1f3d2b052148514a4c2d03dc0cb2f4653bd40c25
otworiallan <allanotwori4@gmail.com>
2022-03-10 13:48:41 -0800

    Programming is like a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
 
 {
-	char *s = "\"Programming is like building a multilingual puzzle";
+	char *s = "/"Programming is like building a multilingual puzzle";
+	
 	puts(s);
-	return (0);
+
+	return(0);
 }
