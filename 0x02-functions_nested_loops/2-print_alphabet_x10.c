b4698e7a0f110504f933528adaafa6a75cf4c618
otworiallan <allanotwori4@gmail.com>
2022-03-15 11:16:27 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - 10 times alphabet, in lowercase with
+*
+* Return: Always 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+int i;
+char alphabet;
+
+for (i = 0; i <= 9; i++)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+}
+}
